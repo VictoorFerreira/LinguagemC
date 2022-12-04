e329be58dce53e97d535251dde0820784b0a7125
vhfsilva <30941086+VictoorFerreira@users.noreply.github.com>
2022-12-04 11:21:50 -0300

    Curso

---
+++ Ex000.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <locale.h>
+void main(){
+    setlocale(LC_ALL, "portuguese");
+    printf("Olá, Mundo!\n");
+    printf("Ufa! Já Estou \"LIVRE\" da maldição.\n");
+}
---
+++ ex002.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+	setlocale(LC_ALL, "portuguese");
+	printf("\n==================================\n");
+	printf("\\a \t=\t Beep \n");
+	printf("\\n \t=\t Nova Linha \n");
+	printf("\\t \t=\t Tabulação \n");
+	printf("\\\\ \t=\t Barra \n");
+	printf("%%%% \t=\t Porcentagem \n");
+	printf("\\\? \t=\t Interrogação \n");
+	printf("==================================\n");
+}
